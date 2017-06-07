18f661970945734fe8efcef3cb8b0abe69f7cd74
Vanderlei Silva <vanderleisilva@santos.sp.gov.br>
2017-06-07 11:26:38 -0300

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
+# vue-bootstrap-validate 
+
 > A bootstrap integrated validation component for vue.js.
 
+The component adds a bootstrap style to the validation form elements status. The component uses **vee-validate** to perform validations.  
+
+# Requirements
+
+- [Vue.js](https://github.com/vuejs/vue) `^2.0.0`
+- Module bundler: [webpack](https://github.com/webpack/webpack)
+- [Bootstrap](getbootstrap.com) `^3.3.7`
+- [Vee-validate](http://vee-validate.logaretm.com/) `^2.0.0`
+
+# Installation
+
+``` bash
+$ npm install vue-bootstrap-validate --save
+```
+
+# Usage
+``` html
+<field-validation :status="errors" :custom-errors="apiErrors" label="Input label" field="data">
+	<input v-validate="'required'" class="form-control" v-model="databind" name="data" data-vv-as="data-to-show" type="text" />
+</field-validation> 
+
+<script>
+import fieldValidation from 'vue-bootstrap-validate'
+
+import ptBR from 'vee-validate/dist/locale/pt_BR'
+fieldValidation.locale(ptBR, 'pt_BR')
+
+Vue.component('field-validation', fieldValidation);
+</script>
+```
--- src/bootstrap-validate.vue
+++ src/bootstrap-validate.vue
@@ -1,7 +1,53 @@
 <template>
-
+  <div class="form-group has-feedback" :class="[problems ? 'has-error' : '', cssClass]">
+      <label v-show="label" class="control-label" :for="field">{{label}}</label>
+      <slot />      
+      <span v-show="problems && label" class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>
+      <span v-show="problems" class="help-block">
+        <p v-for="problem in problems">
+          <i v-show="problems.length > 1" class="fa fa-exclamation" aria-hidden="true"></i>
+          {{ problem }}
+        </p>
+      </span>
+    </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import VeeValidate,  { Validator } from 'vee-validate'
 
+Vue.use(VeeValidate, {
+  fieldsBagName: 'veeFields'
+});
+
+export default {
+  name: "field-validation",
+  computed: {
+    problems() { 
+      var result = [];
+      if (this.status.has(this.field)) { result.push(this.status.first(this.field)) }
+
+      if (this.customErrors && this.customErrors[this.field]) { 
+        this.customErrors[this.field].forEach(item => { result.push(item); });
+      }
+
+      return result.length > 0 ? result : false;
+    },
+  },
+  props: {
+    status: Object,
+    field: String,
+    label: String,
+    cssClass: String,
+    customErrors: Object
+  },
+  locale(lang, label){   
+    Validator.addLocale(lang);
+    Validator.setLocale(label);
+  }
+} 
 </script>
+
+<style scoped>
+  .help-block p{ margin:0; }
+</style>
